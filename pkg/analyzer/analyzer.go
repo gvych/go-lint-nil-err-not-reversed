790c777b92e28113fd60b1a3cf242fa42854de7b
Vyacheslav Gusev <vyacheslav.gusev@gmail.com>
2021-05-21 01:08:38 +0000

    switched to only "err != nil" expression

--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -1,7 +1,6 @@
 package analyzer
 
 import (
-	"fmt"
 	"go/ast"
 	"go/token"
 
@@ -44,21 +43,11 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	inspector.Preorder(nodeFilter, func(node ast.Node) {
 		ifDecl := node.(*ast.IfStmt)
 
-
-		//res, _ := ifDecl.Cond.(*ast.BinaryExpr)
-		if res, _ := ifDecl.Cond.(*ast.BinaryExpr); res != nil && res.Op == token.NEQ {// && res.Y == "nil" {
-	    //fmt.Printf("hello world %T\n",res)
-	    //fmt.Printf("hello world %s\n",res)
+		if res, _ := ifDecl.Cond.(*ast.BinaryExpr); res != nil && res.Op == token.NEQ {
+			x, _ := res.X.(ast.Expr).(*ast.Ident)
 			y, _ := res.Y.(ast.Expr).(*ast.Ident)
-			//if y == nil { return }
-	    fmt.Printf("hello world %T\n",y.Name)
-	    fmt.Printf("hello world %s\n",y.Name)
-			//x, _ := y.(*ast.Ident)
-	    //fmt.Printf("hello world %T\n",x)
-	    //fmt.Printf("hello world %s\n",x)
-			//if x == nil && x.Name == "nil" { return }
-	    //fmt.Printf("hello world %T\n",y.Name)
-	    //fmt.Printf("hello world %s\n",y.Name)
+			if x.Name != "err" || y.Name != "nil" { return } //only constructions "if err != nill" accepted
+
 			blockStmt := ifDecl.Body
 			if blockStmt != nil {
 				v := visitor{}
--- testdata/src/p/p.go
+++ testdata/src/p/p.go
@@ -5,30 +5,30 @@ import "fmt"
 
 func funcWithReversedErrHandling() {
 	err := errors.New("error")
-	   //do nothing
 	if err != nil {
-	   fmt.Println("hello world")
+		//"err" not used
+		fmt.Println("hello world")
 	} else {
 		//"err" usage in wrong place
-	  fmt.Println(err)
+		fmt.Println(err)
 	}
 
 	if err != nil {
 		//"err" usage in Right place
-	  fmt.Println(err)
-	   fmt.Println("hello world")
+		fmt.Println(err)
+		fmt.Println("hello world")
 	} else {
-	   //do nothing
-	   fmt.Println("hello world")
+		//"err" not used in else, as it should
+		fmt.Println("hello world")
 	}
 
 	i := 1
 	if i != 2 {
-	  fmt.Println("skipped block")
+		fmt.Println("skipped block")
 	}
 
-  core := "test"
+	core := "test"
 	if core != "" {
-	  fmt.Println("skipped block")
+		fmt.Println("skipped block")
 	}
 }
